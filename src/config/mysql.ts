5d1d270eb33ae68fe3f28597ee063a21a4dd6c8c
Andrewkizito <Andrewkizito54@gmail.com>
2023-05-15 22:26:34 +0300

    SQL Setup

---
+++ src/config/mysql.ts
@@ -0,0 +1,15 @@
+// Importing dependencies
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+//Loading environment
+dotenv.config()
+
+const sql_connection = mysql.createConnection({
+	database: process.env.db_name,
+	user: process.env.db_user,
+	password: process.env.db_password,
+	host: process.env.db_host,
+})
+
+export default sql_connection
